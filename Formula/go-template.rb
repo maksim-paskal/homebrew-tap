4cc4e8a88a2a2b50aa45c4b9efad6cc39e1c411e
devops.maksim.p <devops.maksim.p@alldigitalads.com>
2020-12-05 16:41:41 +0200

    test

--- Formula/go-template.rb
+++ Formula/go-template.rb
@@ -1,14 +1,17 @@
 class GoTemplate < Formula
   desc ""
   homepage ""
-  url "https://github.com/maksim-paskal/go-template/releases/download/v0.0.6/go-template-darwin-amd64"
-  sha256 "389ed0e0a0c7009e7de89bd0412fda33dc10d5b50e410e3a0891fe83033a35cb"
+
+  if OS.mac?
+    url "https://github.com/maksim-paskal/go-template/releases/download/v0.0.6/go-template-darwin-amd64"
+    sha256 "389ed0e0a0c7009e7de89bd0412fda33dc10d5b50e410e3a0891fe83033a35cb"
+  end
 
   def install
     bin.install "./go-template-darwin-amd64"
   end
 
   test do
-    system "false"
+    system "#{bin}/go-template --version"
   end
 end
