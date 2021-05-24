56b302b01c64b485a7dc744908d440dd1c0d0af4
devops.maksim.p <devops.maksim.p@alldigitalads.com>
2021-05-24 09:01:49 +0300

    v0.0.3

--- Formula/hcloud-k8s-ctl.rb
+++ Formula/hcloud-k8s-ctl.rb
@@ -1,11 +1,11 @@
 class HcloudK8sCtl < Formula
   desc ""
   homepage ""
-  version "v0.0.2"
+  version "v0.0.3"
 
   if OS.mac?
-    url "https://github.com/maksim-paskal/hcloud-k8s-ctl/releases/download/v0.0.2/hcloud-k8s-ctl-darwin-amd64"
-    sha256 "53001e556edc1f6ab76caf71778378cb38885dc078aad0505c432d993aa76d04"
+    url "https://github.com/maksim-paskal/hcloud-k8s-ctl/releases/download/v0.0.3/hcloud-k8s-ctl-darwin-amd64"
+    sha256 "a94466fbe638c8e3f4e7e352e1cb86991ed47856725ec3245820aee3b790b4fb"
   end
 
   def install
--- README.md
+++ README.md
@@ -1,3 +1,9 @@
+## https://github.com/maksim-paskal/go-template
 ```
 brew install maksim-paskal/tap/go-template
+```
+
+## https://github.com/maksim-paskal/hcloud-k8s-ctl
+```
+brew install maksim-paskal/tap/hcloud-k8s-ctl
 ```
