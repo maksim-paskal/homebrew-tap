91c130d87104ee8dcaf8b1f6426f70fbcefe3282
devops.maksim.p <devops.maksim.p@alldigitalads.com>
2021-06-01 09:43:31 +0300

    v0.0.7

--- Formula/hcloud-k8s-ctl.rb
+++ Formula/hcloud-k8s-ctl.rb
@@ -1,11 +1,11 @@
 class HcloudK8sCtl < Formula
   desc ""
   homepage ""
-  version "v0.0.6"
+  version "v0.0.7"
 
   if OS.mac?
-    url "https://github.com/maksim-paskal/hcloud-k8s-ctl/releases/download/v0.0.6/hcloud-k8s-ctl-darwin-amd64"
-    sha256 "caf702d8bf54d709ce3fac99a3c78d613e4e027f63bd39ad3f6bdd473b676413"
+    url "https://github.com/maksim-paskal/hcloud-k8s-ctl/releases/download/v0.0.7/hcloud-k8s-ctl-darwin-amd64"
+    sha256 "5a5510c1f1f79cd7714efeabe8fa13a2080317510352d19a1ed9e2cbd005184c"
   end
 
   def install
