bd891dc45fe7dd4ab8440e86d05356cd722c3ebb
Maksim Paskal <paskal.maksim@gmail.com>
2021-07-08 09:55:02 +0300

    add aks-engine
    Signed-off-by: Maksim Paskal <paskal.maksim@gmail.com>

---
+++ Formula/aks-engine.rb
@@ -0,0 +1,20 @@
+class AksEngine < Formula
+  desc ""
+  homepage ""
+  version "v0.65.0"
+
+  if OS.mac?
+    url "https://github.com/maksim-paskal/homebrew-tap/releases/download/binary/aks-engine"
+    sha256 "b1d3b258c76d0fb0c787e5543a1f4067079af22791c1ab20323e376157d7aaf9"
+  end
+
+  def install
+    if OS.mac?
+      bin.install "./aks-engine" => "aks-engine"
+    end
+  end
+
+  test do
+    system "#{bin}/aks-engine version"
+  end
+end
