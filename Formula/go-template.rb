a29e5c893c548e625e24581afaf550ff3425b264
Maksim Paskal <paskal.maksim@gmail.com>
2021-07-30 12:36:32 +0300

    go-template v0.0.9
    Signed-off-by: Maksim Paskal <paskal.maksim@gmail.com>

--- Formula/go-template.rb
+++ Formula/go-template.rb
@@ -1,11 +1,11 @@
 class GoTemplate < Formula
   desc ""
   homepage ""
-  version "v0.0.8"
+  version "v0.0.9"
 
   if OS.mac?
-    url "https://github.com/maksim-paskal/go-template/releases/download/v0.0.8/go-template-darwin-amd64"
-    sha256 "b985f2a8fcf095d6de8cb7fa78dbbf16b9a888fba799876813020726a9dc50fd"
+    url "https://github.com/maksim-paskal/go-template/releases/download/v0.0.9/go-template-darwin-amd64"
+    sha256 "b5243c866c7c3129f9c2a3bf756ba1b17ea163b542dc795670e42e1e5118b3bf"
   end
 
   def install
