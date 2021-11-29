f8c626ebd432c88d6ebad8476d9b5d2f10350a0d
Maksim Paskal <paskal.maksim@gmail.com>
2021-11-29 15:33:50 +0200

    add 0.1.2
    Signed-off-by: Maksim Paskal <paskal.maksim@gmail.com>

--- Formula/hcloud-k8s-ctl.rb
+++ Formula/hcloud-k8s-ctl.rb
@@ -1,11 +1,11 @@
 class HcloudK8sCtl < Formula
   desc ""
   homepage "https://github.com/maksim-paskal/hcloud-k8s-ctl"
-  version "0.1.1"
+  version "0.1.2"
 
   if OS.mac?
     url "https://github.com/maksim-paskal/hcloud-k8s-ctl/releases/download/v#{version}/hcloud-k8s-ctl_#{version}_darwin_amd64"
-    sha256 "c53ad019bff7d147e5851b7a24fbd36f9befdf029e82240761e4e3920b07ff71"
+    sha256 "2fcda82f9b5bb3ac35e28211b4ad0d7707564aa4701f88458c11d85b509014ee"
   end
 
   def install
