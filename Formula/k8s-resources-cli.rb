722aeb3483b606e01b89e1ad043df9c0e40cf407
Maksim Paskal <paskal.maksim@gmail.com>
2022-04-23 08:49:35 +0300

    up
    Signed-off-by: Maksim Paskal <paskal.maksim@gmail.com>

--- Formula/k8s-resources-cli.rb
+++ Formula/k8s-resources-cli.rb
@@ -1,11 +1,11 @@
 class K8sResourcesCli < Formula
   desc ""
   homepage "https://github.com/maksim-paskal/k8s-resources-cli"
-  version "0.0.4"
+  version "0.0.5"
 
   if OS.mac?
     url "https://github.com/maksim-paskal/k8s-resources-cli/releases/download/v#{version}/k8s-resources-cli_#{version}_darwin_amd64"
-    sha256 "aaed74e7c08612a7ff796119f6ede7b961570e712bf8421af30c79667accf582"
+    sha256 "f590dc5f320f7bc33106c709126f72d3b83e1d84dd5e10187ec5b47e434108a2"
   end
 
   def install
