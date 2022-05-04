ac5c9a57737df2c9a71f885e201898520e52f521
Maksim Paskal <paskal.maksim@gmail.com>
2022-05-04 07:21:04 +0300

    new version
    Signed-off-by: Maksim Paskal <paskal.maksim@gmail.com>

--- Formula/k8s-resources-cli.rb
+++ Formula/k8s-resources-cli.rb
@@ -1,11 +1,11 @@
 class K8sResourcesCli < Formula
   desc ""
   homepage "https://github.com/maksim-paskal/k8s-resources-cli"
-  version "0.0.5"
+  version "0.0.6"
 
   if OS.mac?
     url "https://github.com/maksim-paskal/k8s-resources-cli/releases/download/v#{version}/k8s-resources-cli_#{version}_darwin_amd64"
-    sha256 "f590dc5f320f7bc33106c709126f72d3b83e1d84dd5e10187ec5b47e434108a2"
+    sha256 "63423c00696e1793d215752ad6292c006c13243df820975c38af7964f69f1805"
   end
 
   def install
--- Makefile
+++ Makefile
@@ -1,6 +1,5 @@
 test:
 	brew install --debug --verbose \
-	./Formula/aks-engine.rb \
 	./Formula/k8s-resources-cli.rb \
 	./Formula/go-template.rb \
 	./Formula/hcloud-k8s-ctl.rb
@@ -8,7 +7,6 @@ clean:
 	brew uninstall `brew ls --full-name --formula | grep maksim-paskal/tap` || true
 	brew untap maksim-paskal/tap || true
 	brew uninstall --force \
-	aks-engine \
 	k8s-resources-cli \
 	go-template \
 	hcloud-k8s-ctl
