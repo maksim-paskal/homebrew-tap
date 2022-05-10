91718d92bfd92062a319732cf9de60aa79c5e5f1
Maksim Paskal <paskal.maksim@gmail.com>
2022-05-10 15:14:26 +0300

    fix hash
    Signed-off-by: Maksim Paskal <paskal.maksim@gmail.com>

--- Formula/k8s-resources-cli.rb
+++ Formula/k8s-resources-cli.rb
@@ -5,7 +5,7 @@ class K8sResourcesCli < Formula
 
   if OS.mac?
     url "https://github.com/maksim-paskal/k8s-resources-cli/releases/download/v#{version}/k8s-resources-cli_#{version}_darwin_amd64"
-    sha256 "63423c00696e1793d215752ad6292c006c13243df820975c38af7964f69f1805"
+    sha256 "80f07f84997c60e6fecc793c55393797f8b6b5226e9b37c1d269e00301154145"
   end
 
   def install
