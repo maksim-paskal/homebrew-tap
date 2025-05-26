e17c2010c60f72f910a71b584a17b24523df5063
Maksim Paskal <paskal.maksim@gmail.com>
2025-05-26 18:13:25 +0200

    feat: Update version
    Signed-off-by: Maksim Paskal <paskal.maksim@gmail.com>

--- Formula/boundary-cli.rb
+++ Formula/boundary-cli.rb
@@ -1,23 +1,23 @@
 class BoundaryCli < Formula
   desc "Helper tool for HashiCorp Boundary"
   homepage "https://github.com/maksim-paskal/boundary-cli"
-  version "0.0.1"
+  version "0.0.2"
 
   livecheck do
     url :homepage
     regex(/^v?(\d+(?:\.\d+)+)$/i)
   end
 
-  # curl -L https://github.com/maksim-paskal/boundary-cli/releases/download/v0.0.1/checksums.txt
+  # curl -L https://github.com/maksim-paskal/boundary-cli/releases/download/v0.0.2/checksums.txt
   if OS.mac?
     if Hardware::CPU.intel?
       url "https://github.com/maksim-paskal/boundary-cli/releases/download/v#{version}/boundary-cli_#{version}_darwin_amd64.tar.gz"
-      sha256 "c79d4eb40545d7a896ee444de972870c948c5b93c21dc569664a6ff1560a5872"
+      sha256 "c6c1ad06b3b4b7b282392d6efabdf88a5339776f9f6ad88e738e97a9207b7869"
     end
 
     if Hardware::CPU.arm?
       url "https://github.com/maksim-paskal/boundary-cli/releases/download/v#{version}/boundary-cli_#{version}_darwin_arm64.tar.gz"
-      sha256 "1051647a2d2c748e6e556bbbfa769f3291908813fddebc81630884406301baca"
+      sha256 "064c8cb3358ed7c5ff0c8ebdde8dc28484e731d8093cfb5dc094e46581b5d16c"
     end
   end
 
